6294ab9913702862dc071b46239e779c74d4f737
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-12-30 04:41:01 +0000

    start

---
+++ src/main/java/com/ryancodes/sfgwebapp/SfgwebappApplication.java
@@ -0,0 +1,13 @@
+package com.ryancodes.sfgwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryancodes/sfgwebapp/SfgwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryancodes.sfgwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
