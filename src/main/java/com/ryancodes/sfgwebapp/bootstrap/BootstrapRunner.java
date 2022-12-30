bf98e995e114a010a852c1200519d301a7c40a9b
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-12-30 07:58:16 +0000

    update

--- src/main/java/com/ryancodes/sfgwebapp/bootstrap/BootstrapRunner.java
+++ src/main/java/com/ryancodes/sfgwebapp/bootstrap/BootstrapRunner.java
@@ -1,7 +1,14 @@
 package com.ryancodes.sfgwebapp.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+
+
+@Component
 public class BootstrapRunner implements CommandLineRunner {
-
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Bootstrapping data");
+    };
 }
