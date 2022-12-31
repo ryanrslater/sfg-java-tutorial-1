bb18c6d9961e911a9d28e6eb298dfc7bd47f4d22
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-12-31 05:39:09 +0000

    update

--- src/main/java/com/ryancodes/sfgwebapp/domain/Publisher.java
+++ src/main/java/com/ryancodes/sfgwebapp/domain/Publisher.java
@@ -11,4 +11,25 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Publisher() {
+    }
+    public Publisher(String name, String addressLine1, String city, String state, String zip) {
+        this.name = name;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
